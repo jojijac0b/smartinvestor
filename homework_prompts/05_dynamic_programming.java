79fadbfc8fff24638d0dad9d4bb78a5a6a6009e1
jojijac0b <jojijacob22@gmail.com>
2018-04-01 21:35:22 -0700

    Update 05_dynamic_programming.java

--- homework_prompts/05_dynamic_programming.java
+++ homework_prompts/05_dynamic_programming.java
@@ -62,8 +62,17 @@ class Problems {
   // Auxiliary Space Complexity:
 
   public static int coinSum(int[] coins, int total) {
-    //YOUR WORK HERE
-    return -1;
+    int[][] dp = new int[coins.length+1][total+1];
+    dp[0][0] = 1;
+    
+    for(int i = 1; i < dp.length; i++){
+      for(int j = 0; j < dp[0].length; j++){
+        dp[i][j] = dp[i-1][j];
+        if(j >= coins[i-1])dp[i][j] += dp[i][j-coins[i-1]];
+      } 
+    }
+    return dp[dp.length-1][total];
+    
   }
 
 }
